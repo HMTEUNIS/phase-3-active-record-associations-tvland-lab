77fb18088009b7c3795a8103b38bf022fd59156a
Holly Teunis <+>
2021-12-01 23:49:49 -0500

    commit tree d3b022f77e2488143da62b6d7ba8eba8cfd069a4

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,11 @@
 class Network < ActiveRecord::Base
+  def shows
+    Show.where(network_id: self.id)
+    end
+  def sorry
+    if self.call_letters.downcase == "nbc"
+        "We're sorry about passing on John Mulaney's pilot"
+    end
   
+  end
 end
